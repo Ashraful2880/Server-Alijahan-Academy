16a00a286eecae7f3cde28993bd2ad2927a72d70
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-19 23:37:35 +0600

    Database Connected With Node App

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+//<------------- Database Code Here ---------->
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxp8q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        //<------------ Database All Collections ------------->
+
+        const database = client.db("Ali-Jahan-Academy");
+        const events = database.collection("Events-Collection");
+        const programme = database.collection("Programme-Collection");
+        const testimonial = database.collection("Testimonial-Collections");
+        const tutors = database.collection("Tutors-Collection");
+
+        //<------------ Database All API ------------->
+
+        // Get Events From DB
+        app.get('/events', async (req, res) => {
+            const allEvent = await events.find({}).toArray();
+            res.send(allEvent)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Ali Jahan Academy')
+});
+
+
+app.listen(port, () => {
+    console.log("Running Server Port is", port);
+});
